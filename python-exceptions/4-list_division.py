4c36c1bbd3e239bca090f1da1d73d2462cefede6
Ab.Alsanie4@gmail.com <Ab.Alsanie4@gmail.com>
2025-07-23 11:20:43 +0300

    trying somthing a bit diff

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
+    new_list = [0] * list_length
     for i in range(list_length):
         result = 0
         try:
@@ -12,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("{}".format("out of range"))
         finally:
-            new_list.append(result or 0)
+            new_list[i] = result or 0
     return (new_list)
