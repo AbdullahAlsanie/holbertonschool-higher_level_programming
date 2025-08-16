8c8ae480fafe9835d0588944f6a3b7ba2301f004
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-08-16 13:17:20 +0300

    added sol for task 2 and 3

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,36 +1,35 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
- serialize and deserialize custom Python objects using the pickle module.
- """
-
-
+Serialize classes using pickle
+"""
 import pickle
 
 
 class CustomObject:
-    """ custom Python class """
+    """
+    A serializable class with pickle
+    """
 
-    def __init__(self, name: str, age: int, is_student: bool):
+    def __init__(self, name, age, is_student) -> None:
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """Print the object's attributes in a formatted way."""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
-    def serialize(self, filename: str):
-        """ Serialize the current object instance to a file using pickle. """
-        with open(filename, 'wb') as file:
-            pickle.dump(self, file, protocol=pickle.HIGHEST_PROTOCOL)
+    def serialize(self, filename):
+        with open(filename, mode="wb") as f:
+            pickle.dump(self, f)
 
     @classmethod
-    def deserialize(cls, filename: str):
-        """ Deserialize a CustomObject instance from a file using pickle. """
-        with open(filename, 'rb') as file:
-            obj = pickle.load(file)
-            if not isinstance(obj, cls):
-                return None
-            return obj
+    def deserialize(cls, filename):
+        try:
+            with open(filename, mode="rb") as f:
+                data = pickle.load(f)
+            return cls(name=data.name, age=data.age,
+                       is_student=data.is_student)
+        except Exception:
+            return None
