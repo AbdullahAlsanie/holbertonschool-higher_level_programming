747b9e3518c1291695155db3e90bd2e4ff55ff1e
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-09-03 14:43:41 +0300

    Fixed Task 13 SOL

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,10 @@
 -- this is sub querry using forign key
 SELECT 
-    tv_shows.title,
-    tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres 
-    ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres 
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
