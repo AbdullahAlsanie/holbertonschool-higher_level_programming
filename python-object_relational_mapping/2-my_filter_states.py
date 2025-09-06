151191546fa27b9659c802345a46717d17583ed3
AbdullahAlsanie <Ab.Alsanie4@gmail.com>
2025-09-06 18:07:15 +0300

    fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,41 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-Uses format to create SQL query (vulnerable to SQL injection).
+Takes in an argument and displays all values in the states table where name matches the argument
+Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <state name>
 """
 import sys
 import MySQLdb
 
 
+def filter_states():
+    """
+    Takes in an argument and displays all values in the states table where name matches the argument
+    Usage: ./2-my_filter_states.py <mysql username> <mysql password> <database name> <state name>
+    """
+    try:
+        user1 = sys.argv[1]
+        pass1 = sys.argv[2]
+        db1 = sys.argv[3]
+        match = sys.argv[4]
+        database = None
+        c = None
+        database = MySQLdb.connect(
+            port=3306, host="localhost", user=user1, passwd=pass1, db=db1
+        )
+        c = database.cursor()
+        # Using format() makes this vulnerable to SQL injection as required
+        query = "SELECT * FROM states WHERE name = '{}' " \
+                "ORDER BY states.id ASC".format(match)
+        c.execute(query)
+        [print(state) for state in c.fetchall()]
+    except MySQLdb.Error as e:
+        print(f"Error connecting to database, {e}")
+    finally:
+        if c:
+            c.close()
+        if database:
+            database.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
-    cursor.execute(query)
-    for row in cursor.fetchall():
-        print(row)
-    cursor.close()
-    db.close()
+    filter_states()
